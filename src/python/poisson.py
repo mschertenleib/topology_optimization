14cf1a9c8f283b0af7ada7d57adff226b6793c7a
Mathieu Schertenleib <schertenleib.mathieu@gmail.com>
2024-08-16 23:00:17 +0200

    Complete NGSolve Poisson example

---
+++ src/python/poisson.py
@@ -0,0 +1,32 @@
+import os.path
+import webbrowser
+
+from ngsolve import *
+from ngsolve.webgui import Draw
+
+
+def main() -> None:
+    mesh = Mesh(unit_square.GenerateMesh(maxh=0.1))
+
+    fes = H1(mesh, order=2, dirichlet="bottom|right")
+
+    u = fes.TrialFunction()
+    v = fes.TestFunction()
+    gfu = GridFunction(fes)
+
+    a = BilinearForm(fes)
+    a += grad(u) * grad(v) * dx
+    a.Assemble()
+
+    f = LinearForm(fes)
+    f += x * v * dx
+    f.Assemble()
+
+    gfu.vec.data = a.mat.Inverse(freedofs=fes.FreeDofs()) * f.vec
+
+    Draw(gfu, filename="out.html")
+    webbrowser.open("file://" + os.path.abspath("out.html"))
+
+
+if __name__ == "__main__":
+    main()
