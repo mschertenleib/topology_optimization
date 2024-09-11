e4dbdfa2cccde17148a231b9fe85be5942504cdf
Mathieu Schertenleib <schertenleib.mathieu@gmail.com>
2024-09-11 15:08:21 +0200

    Plot analytical deflection

--- src/linear_elasticity_2d.py
+++ src/linear_elasticity_2d.py
@@ -5,18 +5,39 @@ from ngsolve import *
 from netgen.geom2d import SplineGeometry
 from ngsolve.webgui import Draw
 
+import matplotlib.pyplot as plt
+import numpy as np
+
+"""
+[From Gere and Goodno, "Mechanics of Materials"]
+
+Deflection for a horizontal beam with a fixed extremity and a vertical
+load applied at the other extremity (gravity neglected):
+
+v(x) = -F*x^2 / (6*E*I) * (3*L - x)
+
+For a rectangular cross-section beam, I = w*h^3/12
+
+
+"""
+
 
 def main() -> None:
     geo = SplineGeometry()
 
-    p1, p2, p3, p4 = [geo.AppendPoint(x, y) for x, y in [(0, 0), (1, 0), (1, 0.1), (0, 0.1)]]
+    length = 1.0
+    height = 0.1
+    width = 0.1
+    force = -100.0
+
+    p1, p2, p3, p4 = [geo.AppendPoint(x, y) for x, y in [(0, 0), (length, 0), (length, height), (0, height)]]
     geo.Append(["line", p1, p2], bc="bottom")
     geo.Append(["line", p2, p3], bc="right")
     geo.Append(["line", p3, p4], bc="top")
     geo.Append(["line", p4, p1], bc="left")
-    mesh = Mesh(geo.GenerateMesh(maxh=0.02))
+    mesh = Mesh(geo.GenerateMesh(maxh=height / 5.0))
 
-    E = 70  # Young's modulus
+    E = 70e9  # Young's modulus
     nu = 0.35  # Poisson's ratio
 
     # Lamé parameters
@@ -26,8 +47,8 @@ def main() -> None:
     def stress(strain):
         return 2.0 * mu * strain + lam * Trace(strain) * Id(2)
 
-    def strain(u):
-        return Sym(Grad(u))
+    def strain(displacement):
+        return Sym(Grad(displacement))
 
     fes = VectorH1(mesh, order=2, dirichlet="left")
     u = fes.TrialFunction()
@@ -37,14 +58,22 @@ def main() -> None:
     a = BilinearForm(InnerProduct(stress(strain(u)), strain(v)) * dx)
     a.Assemble()
 
-    f = LinearForm(CoefficientFunction((0, -5e-3)) * v * ds("right"))
+    f = LinearForm(CoefficientFunction((0, force)) * v * ds("right"))
     f.Assemble()
 
     inv = a.mat.Inverse(freedofs=fes.FreeDofs(), inverse="sparsecholesky")
     gfu.vec.data = inv * f.vec
 
     Draw(gfu, filename="out.html")
-    webbrowser.open("file://" + os.path.abspath("out.html"))
+    # webbrowser.open("file://" + os.path.abspath("out.html"))
+
+    # Comparison with analytical solution
+    x = np.linspace(0.0, 1.0, 1000)
+    I_z = width * height**3 / 12.0
+    v = force * x * x / (6.0 * E * I_z) * (3.0 * length - x)
+    fig, ax = plt.subplots()
+    ax.plot(x, v)
+    fig.savefig("fig.png")
 
 
 if __name__ == "__main__":
