ded96ca034a12ff63fff3aed906a3b4272431781
Mathieu Schertenleib <schertenleib.mathieu@gmail.com>
2025-02-08 23:15:14 +0100

    Create quad mesh

--- src/mesh.py
+++ src/mesh.py
@@ -6,41 +6,46 @@ from netgen.meshing import *
 from ngsolve.webgui import Draw
 
 
-def main() -> None:
-    ngmesh = Mesh(dim=2)
-    N = 5
+def create_quad_mesh(size_x: float, size_y: float, nx: int, ny: int) -> Mesh:
+    mesh = Mesh(dim=2)
 
     point_ids = []
-    for i in range(N + 1):
-        for j in range(N + 1):
-            point_ids.append(ngmesh.Add(MeshPoint(Pnt(i / N, j / N, 0))))
+    for iy in range(ny + 1):
+        for ix in range(nx + 1):
+            point_ids.append(mesh.Add(MeshPoint(Pnt(ix / nx * size_x, iy / ny * size_y, 0))))
 
-    domain_index = ngmesh.AddRegion("region", dim=2)
-    for j in range(N):
-        for i in range(N):
-            ngmesh.Add(
+    domain_index = mesh.AddRegion("region", dim=2)
+    for iy in range(ny):
+        for ix in range(nx):
+            mesh.Add(
                 Element2D(
                     domain_index,
                     [
-                        point_ids[i + j * (N + 1)],
-                        point_ids[i + (j + 1) * (N + 1)],
-                        point_ids[i + 1 + (j + 1) * (N + 1)],
-                        point_ids[i + 1 + j * (N + 1)],
+                        point_ids[iy * (nx + 1) + ix],
+                        point_ids[iy * (nx + 1) + ix + 1],
+                        point_ids[(iy + 1) * (nx + 1) + ix + 1],
+                        point_ids[(iy + 1) * (nx + 1) + ix],
                     ],
                 )
             )
 
-    for i in range(N):
-        ngmesh.Add(Element1D([point_ids[i], point_ids[i + 1]], index=1))
-        ngmesh.Add(
-            Element1D([point_ids[N + i * (N + 1)], point_ids[N + (i + 1) * (N + 1)]], index=2)
+    for ix in range(nx):
+        mesh.Add(Element1D([point_ids[ix], point_ids[ix + 1]], index=1))
+        mesh.Add(
+            Element1D([point_ids[ny * (nx + 1) + ix], point_ids[ny * (nx + 1) + ix + 1]], index=3)
         )
-        ngmesh.Add(Element1D([point_ids[i + N * (N + 1)], point_ids[i + 1 + N * (N + 1)]], index=3))
-        ngmesh.Add(
-            Element1D([point_ids[0 + i * (N + 1)], point_ids[0 + (i + 1) * (N + 1)]], index=4)
+    for iy in range(ny):
+        mesh.Add(
+            Element1D([point_ids[iy * (nx + 1) + nx], point_ids[(iy + 1) * (nx + 1) + nx]], index=2)
         )
+        mesh.Add(Element1D([point_ids[iy * (nx + 1)], point_ids[(iy + 1) * (nx + 1)]], index=4))
 
-    mesh = ngsolve.Mesh(ngmesh)
+    return mesh
+
+
+def main() -> None:
+    mesh = create_quad_mesh(size_x=3.0, size_y=1.5, nx=20, ny=10)
+    mesh = ngsolve.Mesh(mesh)
     Draw(mesh, filename="out.html")
     webbrowser.open("file://" + os.path.abspath("out.html"))
 
